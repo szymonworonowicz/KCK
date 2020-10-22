1f733330fcaddfef76796264e94744c048d78437
szymon <szymonworonowicz25@gmail.com>
2020-10-22 02:41:59 +0200

    first commit

---
+++ KCKProjectAPI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KCKProjectAPI
+{
+    public class Class1
+    {
+    }
+}
---
+++ KCKProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KCKProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
