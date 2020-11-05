96a20cd0f169d5e6eba5472417d31cb6958cafaf
szymon <szymonworonowicz25@gmail.com>
2020-11-05 17:50:51 +0100

    update api target

---
+++ KCKProjektAPI/Builders/WPFBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace KCKProjectAPI.Builders
+{
+    class WPFBuilder
+    {
+
+    }
+}
